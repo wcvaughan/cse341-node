b2f7ab435f2ec4b0c98c3a4fab4406c8734f9a6e
wcvaughan <chrisvaughanlds@gmail.com>
2025-03-06 13:18:21 -0900

    work work work

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node running at port ${port}`);
+        });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Web Server is listening at port ${port}`);
-});
